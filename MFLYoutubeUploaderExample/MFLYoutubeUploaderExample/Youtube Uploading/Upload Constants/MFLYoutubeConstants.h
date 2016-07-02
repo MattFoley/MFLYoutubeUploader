4345eb725a60c15d2560e7f7f9222f248b6f655f
Tj <tfallon@mail.depaul.edu>
2016-07-02 15:33:11 -0700

    Comment out keys in main line.

--- MFLYoutubeUploaderExample/MFLYoutubeUploaderExample/Youtube Uploading/Upload Constants/MFLYoutubeConstants.h
+++ MFLYoutubeUploaderExample/MFLYoutubeUploaderExample/Youtube Uploading/Upload Constants/MFLYoutubeConstants.h
@@ -8,8 +8,14 @@
 
 #import <Foundation/Foundation.h>
 
+ 
+//In order to compile, uncomment these lines and insert your own values
+
+/*
 static NSString * const kYTClientID             = @"<replace with your client id>";
 static NSString * const kYTClientSecret         = @"<replace with your secret>";
+*/
+
 static NSString * const kYTKeychainItemName     = @"UniqueKeychainItemName";
 
 //Currently not used.
